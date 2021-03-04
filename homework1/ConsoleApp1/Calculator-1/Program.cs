d19e3bac044febc5081cb4b6332bc75aaec2b4e6
lixj111 <79953753+lixj111@users.noreply.github.com>
2021-03-04 22:46:56 +0800

    Add files via upload

---
+++ homework1/ConsoleApp1/Calculator-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double x = 0;
+            double y = 0;
+            Console.Write("Please input a double number x:");
+            s = Console.ReadLine();
+            x = Double.Parse(s);
+            Console.Write("Please input a double number y:");
+            s = Console.ReadLine();
+            y = Double.Parse(s);
+            Console.Write("Please input an operator to calculate x and y:");
+            s = Console.ReadLine();
+            Console.Write("x:{0},s:{1},y:{2},结果为:", x, s, y);
+            if (s == "+")
+                Console.WriteLine(x+y);
+            else if (s == "-")
+                Console.WriteLine(x-y);
+            else if (s == "*")
+                Console.WriteLine(x*y);
+            else if (s == "/")
+                Console.WriteLine(x/y);
+            else
+                Console.WriteLine("Error.");
+        }
+    }
+}
---
+++ homework1/ConsoleApp1/Calculator-2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y, z;
+            x = Convert.ToSingle(textBox1.Text);
+            y = Convert.ToSingle(textBox2.Text);
+            z = x + y;
+            textBox3.Text = z.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double x, y, z;
+            x = Convert.ToSingle(textBox1.Text);
+            y = Convert.ToSingle(textBox2.Text);
+            z = x - y;
+            textBox3.Text = z.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double x, y, z;
+            x = Convert.ToSingle(textBox1.Text);
+            y = Convert.ToSingle(textBox2.Text);
+            z = x * y;
+            textBox3.Text = z.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double x, y, z;
+            x = Convert.ToSingle(textBox1.Text);
+            y = Convert.ToSingle(textBox2.Text);
+            z = x / y;
+            textBox3.Text = z.ToString();
+        }
+    }
+}
